42b08e57e4acef1006359801fe0c8e836291dfcb
Carlos López Martínez <carlos.lopezm@tinamica.com>
2022-11-29 11:29:04 +0100

    feat: Test imports

--- tests/mymodule/test_funcs.py
+++ tests/mymodule/test_funcs.py
@@ -1,21 +1,21 @@
 import pytest
 
-from myapp.mymodule.funcs import *
+import myapp.mymodule.funcs as F
 
 
 @pytest.mark.easy_operation
 def test_add():
     # This test will fail.
-    assert add(4, 8) == 12
+    assert F.add(4, 8) == 12
 
 @pytest.mark.easy_operation
 def test_subtract():
-    assert subtract(3, 6) == -3
+    assert F.subtract(3, 6) == -3
 
 @pytest.mark.difficult_operation
 def test_multiply():
-    assert multiply(4, 5) == 20
+    assert F.multiply(4, 5) == 20
 
 @pytest.mark.difficult_operation
 def test_divide():
-    assert divide(56, 8) == 7
+    assert F.divide(56, 8) == 7
